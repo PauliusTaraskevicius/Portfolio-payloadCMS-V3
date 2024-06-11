3f8a5bb1b2ad2d42f2be99d603e2731dcf3db097
PauliusTaraskevicius <fixas1029@gmail.com>
2024-06-11 09:32:10 +0300

    first commit

---
+++ src/app/(app)/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import { Button } from "@/components/ui/button"
+
+export default function Home() {
+  return (
+    <div className="p-72">
+      <Button variant="destructive">Button</Button>
+    </div>
+    
+  );
+}
